7e577cbc5f723081191a6abc6f7519ef37a29234
sefabckn <sefabckn@gmail.com>
2021-10-25 15:12:38 +0200

    ex-7 solved

--- homework.js
+++ homework.js
@@ -56,7 +56,14 @@ console.log(newStringCreator("strive", 3))
 
 /* 7. Create a function to display the city name if the string begins with "Los" or "New". 
     Pass the city name as a parameter. Return `false` if they start with a different string. */
-
+const cityNameShow = function(cityName) {
+    if (cityName.slice(0, 3) === "Los" || cityName.slice(0, 3) === "New") {
+        return cityName
+    } else {
+        return false
+    }
+}
+console.log(cityNameShow("Amsterdam"))
 
 /* 8. Create a function to calculate and return the sum of all elements from an array with 3 elements. 
     Pass the array as a parameter. */
